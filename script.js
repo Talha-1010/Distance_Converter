c0d035d73af43601f8ef2fb4c7eab8193674a464
Talha - <talhanasr3e@gmail.com>
2021-07-30 05:29:55 +0500

    created two way conversion

--- script.js
+++ script.js
@@ -2,26 +2,77 @@
 
 "use strict";
 
-// 1 mile = 1.60934 km
-document.getElementById('convert').addEventListener('submit',function(event){
-			
-    event.preventDefault();
-    var distanceInMiles= document.getElementById('distance').value;
-    distanceInMiles=parseFloat(distanceInMiles);
-    var ans = document.getElementById('answer');
-    if(distanceInMiles)
-    {//if distanceInMiles is a natural number
-    var distanceInKm= distanceInMiles* 1.60934;
-    var roundedDistanceInKm= distanceInKm.toFixed(3);//rounding of the result to 3 decimal places
+//declaring variables
+var convertType = "convertType";
+var form = document.getElementById('convert');
     
-    ans.innerHTML = `<h2> ${distanceInMiles} miles is equal to ${roundedDistanceInKm} kilometers </h2>`
+
+    function conversion(event){
+                
+        event.preventDefault();
+        var distance= document.getElementById('distance').value;
+        distance=parseFloat(distance);
+        var ans = document.getElementById('answer');
+        
+        if(distance  )
+        {//if distance is a natural number
+            if(convertType=="kilometers")
+            {
+                var distanceInMiles= (distance/1.60934);
+                var roundedDistanceInMiles= distanceInMiles.toFixed(3);//rounding of the result to 3 decimal places
+                
+                ans.innerHTML = `<h2> ${distance} kilometers is equal to ${roundedDistanceInMiles} Miles </h2>`
+            }
+            else{
+                var distanceInKm= distance* 1.60934;
+                var roundedDistanceInKm= distanceInKm.toFixed(3);//rounding of the result to 3 decimal places
+                
+                ans.innerHTML = `<h2> ${distance} miles is equal to ${roundedDistanceInKm} kilometers </h2>`
+            }
+        }
+        else
+        {//if distance is an invalid number for e.g a string
+            ans.innerHTML= `<h2> Please enter a valid number</h2>`
+        }
+
+        
     }
-    else
-    {//if distanceInMiles is an invalid number for e.g a string
-        ans.innerHTML= `<h2> Please enter a valid number</h2>`
-    }
+
     
-});
+    
+
+    document.addEventListener('keypress', function(event){
+    
+        var key= event.key;
+        if( (key=="K" || key=="k"))
+        {
+            convertType="kilometers";
+
+            var heading= document.getElementById("h1");
+            heading.innerHTML=`<h1> Kilometers to Miles Converter </h1>`
+
+            var instructions= document.getElementById("p1");
+            instructions.innerHTML=`<p> Type in a number of Kilometers and click the button to convert the distance to Miles. </p>`
+    
+           
+        }
+        else if(key=="M" || key=="m")
+        {   
+            convertType="miles";
+
+            var heading= document.getElementById("h1");
+            heading.innerHTML=`<h1> Miles to Kilometers Converter </h1>`
+
+            var instructions= document.getElementById("p1");
+            instructions.innerHTML=`<p> Type in a number of miles and click the button to convert the distance to kilometers. </p>`
+            
+        }
+        
+
+    });
+
+    
+    form.addEventListener('submit',conversion);
 
 
 })();
